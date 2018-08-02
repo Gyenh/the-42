8e4e3c572aab7db7debef910c2fb1700337f1e1d
wilrib <willy91330@gmail.com>
2018-08-02 23:14:41 +0200

    Derniere modif

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_090857) do
+ActiveRecord::Schema.define(version: 2018_08_02_202032) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
